7f99045b759a21c2acc0edb576ebfd354ed5f73f
Joe <wx104@126.com>
2020-09-23 18:15:26 +0800

    添加design滑动控件

--- app/src/main/java/com/motion/warmcloudweather/MainActivity.java
+++ app/src/main/java/com/motion/warmcloudweather/MainActivity.java
@@ -1,15 +1,30 @@
 package com.motion.warmcloudweather;
 
 import android.Manifest;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.motion.warmcloudweather.location.ILocationContract;
 import com.motion.warmcloudweather.location.LocationPresenter;
 import com.permissionx.guolindev.PermissionX;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import timber.log.Timber;
 
 
